fe38a79de9471ef454ddf5d39803f50b1289eb75
GeneralDido <dimpanouris@gmail.com>
2023-09-01 15:51:27 +0200

    make Writing the default value

--- helper/grades_standards.py
+++ helper/grades_standards.py
@@ -1,3 +1,3 @@
-academic_standards = ['Reading: Literature', 'Reading: Informational Text', 'Writing']
+academic_standards = ['Writing', 'Reading: Literature', 'Reading: Informational Text']
 academic_grades = ['4', '5', '6', '7', '8', '9', '10', '11', '12']
 max_num_questions = 4
