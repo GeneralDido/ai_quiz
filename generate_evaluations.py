537c8c0cc0a7c6aaa66d672fa5d9cbaa317b7f5f
GeneralDido <dimpanouris@gmail.com>
2023-09-01 09:47:00 +0200

    Update generate_evaluations.py

--- generate_evaluations.py
+++ generate_evaluations.py
@@ -21,18 +21,18 @@ def generate_evaluations(grade: str, standard: str, topic: str, questions: list,
         Finally, you should calculate a total grade for the student based on all their responses and provide final feedback.
         If the user has not responded at all to the particular question or has responded randomly, please address it in a nice, instructive way. Remember, your goal is to provide a supportive and constructive learning experience for the student, not just merely grade responses. Be respectful, insightful, creative, and thorough.
         
-        Example output JSON:
+        Return the response only in JSON format (nothing else). Example JSON response:
         {"evaluations":[
             0:{
-                "id":1
-                "grade":5
-                "explanation":"<explanation>"
+                "id":<id>
+                "grade":<grade>
+                "explanation":<explanation>
             }
             1:{ ... }
             2:{ ... }
         ]
-        "finalGrade":4
-        "finalFeedback":"Overall, you've done a good job on this assignment. You demonstrated a strong understanding of Steve Jobs' personality, leadership style, and work ethic, as well as his influence on technology. However, there's still room for improvement. Make sure to answer all parts of the question and don't hesitate to ask questions, even if you're not sure about them. Keep up the good work and continue to strive for excellence. You're on the right path to becoming a better reader and thinker!"
+        "finalGrade":<finalGrade>
+        "finalFeedback":<finalFeedback>
         }
         {{~/system}}
         {{#assistant~}}
